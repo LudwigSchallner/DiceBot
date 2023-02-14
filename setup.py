c8e8747f321efa9c01e9ffd71d4768c96d108876
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-02-14 20:03:34 +0100

    build: init

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Package setup."""
+import sys
+
+from setuptools import setup
+
+try:
+    from semantic_release import setup_hook
+
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+VERSION_TEMPLATE = '''
+"""Automatically generated."""
+__version__ = {version!r}
+'''.lstrip()
+
+setup(
+    use_scm_version={
+        "write_to": "src/dice_bot/version.py",
+        "write_to_template": VERSION_TEMPLATE,
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    },
+)
