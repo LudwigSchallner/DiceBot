b2c4c69e789f76e6fa7efeac816c8863956ebf9b
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-02-14 20:48:35 +0100

    feat(dice): change number to face_number

--- src/dice_bot/dice.py
+++ src/dice_bot/dice.py
@@ -1,3 +1,4 @@
+"""Dice module."""
 import random
 from typing import Mapping, Sequence
 
@@ -7,14 +8,17 @@ import pydantic
 class Dice(pydantic.BaseModel):
     """Dice BaseModel"""
 
-    number: int
+    face_number: int
     command: str
     result_to_text: Mapping[int, str] = {}
+    sum_up: bool = False
 
     def _roll_dice(self) -> int:
         """Roll a 'number' dice."""
-        return random.randint(0, self.number)
+        return random.randint(0, self.face_number)
 
 
 class Dices(Dice):
+    """Data of multiple dices."""
+
     dices: Sequence[Dice]
