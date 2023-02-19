c74ba60257ce6dbd6b87a80ea28302cdbe59df1d
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-02-19 19:41:44 +0100

    tests(dice): update dice test

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -4,4 +4,4 @@ from dice_bot import dice
 def test_dice():
     """Test basic dice behavior."""
     dice_six = dice.Dice(face_number=6, command="roll")
-    assert dice_six._roll_dice() in range(0, 6)
+    assert dice_six() in range(0, 6)
