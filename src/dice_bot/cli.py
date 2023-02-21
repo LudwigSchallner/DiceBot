10346a41ff7049008399e4e18ee0447e48071cc8
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-02-21 16:59:27 +0100

    feat(cli): update cli

--- src/dice_bot/cli.py
+++ src/dice_bot/cli.py
@@ -3,7 +3,7 @@ import pathlib
 import typer
 import yaml
 
-from dice_bot import bot, dice
+from dice_bot import bot
 
 app = typer.Typer()
 
@@ -12,14 +12,20 @@ app = typer.Typer()
 def start(
     config_path: pathlib.Path = typer.Option(..., exists=True, dir_okay=False)
 ) -> None:
+    """Entry point for starting the server and initializing the given config."""
     with open(config_path, mode="r") as file:
         config_file = yaml.safe_load(file)
-    d6 = dice.Dice(face_number=6, name="dmg", alias="d")
 
     config = bot.Config.parse_obj(config_file)
-    client = bot.DiceBot(command_prefix=config.command_prefix)
-    client.add_command(bot.customCommand(name=d6.name, alias=d6.alias, function=d6))
-    client.run(config.token)
+    bot_config = config.bot_config
+    dices = config.dices
+
+    client = bot.DiceBot(command_prefix=bot_config.command_prefix)
+
+    for dice_command in dices:
+        client.add_command(bot.custom_command(dice_command))
+
+    client.run(bot_config.token)
 
 
 if __name__ == "__main__":
