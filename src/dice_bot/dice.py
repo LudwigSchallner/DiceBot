9cd6f2ee7ff75337727581d4ec65199ea6848a63
Ludwig <29692215+LudwigSchallner@users.noreply.github.com>
2023-02-21 17:33:13 +0100

    feat(dice): add formating function+mapping function

--- src/dice_bot/dice.py
+++ src/dice_bot/dice.py
@@ -1,6 +1,6 @@
 """Dice module."""
 import random
-from typing import Mapping
+from typing import Mapping, Sequence
 
 import pydantic
 
@@ -15,7 +15,23 @@ class Dice(pydantic.BaseModel):
 
     def __call__(self, count=1) -> int:
         """Roll `count`number of `face_number` dices."""
-        return [self._roll() for _ in range(count)]
+        rolls = [self._roll() for _ in range(count)]
+        if self.result_to_text:
+            return self._map_result(rolls)
+        return self._format(rolls)
+
+    def _format(self, rolls: Sequence[int]) -> str:
+        if self.sort:
+            rolls = rolls.sort()
+        result_text = ", ".join(rolls)
+        if self.sum_up and len(rolls) > 1:
+            result_sum = sum(rolls)
+            result_text += f" [{result_sum}]"
+        return result_text
+
+    def _map_result(self, rolls: Sequence[int]) -> str:
+        rolls_as_text_list = [self.result_to_text[roll] for roll in rolls]
+        return ", ".join(rolls_as_text_list)
 
     def _roll(self) -> int:
         """Roll `face_number` dice."""
